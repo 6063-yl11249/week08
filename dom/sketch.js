b3f0ae883f23c560b344d726763ff4b628fad88b
Yuanqing <yl11249@nyu.edu>
2023-10-28 17:50:25 -0400

    dom

--- dom/sketch.js
+++ dom/sketch.js
@@ -1,7 +1,19 @@
+let slider;
+let cp;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  slider = createSlider(0,width,10)
+  slider.position(100,100);
+
+  cp = createColorPicker ("gold");
+  cp.position(100,300);
 }
 
 function draw() {
-  background(255, 55, 155);
+  background(cp.color());
+  let currentVal = slider.value();
+  ellipse(width/2,height/2,currentVal,currentVal);
+
+
 }
--- sound/sketch.js
+++ sound/sketch.js
@@ -7,8 +7,31 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   print(song.duration());
+  song.playmode("restart");
 }
 
 function draw() {
-  background(220, 20, 120);
+  
+  if (song.isPlaying()) {
+    background ("gold")
+  }else if (song.isPaused()) {
+    background ("blue")
+  }else{
+    background(220, 20, 120);
+  }
+  
+}
+
+function mouseClicked() {
+  song.play();
+}
+
+function keyReleased() {
+  if (key === "s") (
+    song.stop()
+  ) else if (key == "p") {
+    if (song.isPlaying()) {
+song.pause();
+    }
+  }
 }
